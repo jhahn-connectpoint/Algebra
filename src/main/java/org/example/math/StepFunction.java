233af8d781d7f9e6ee995cea05c92786f4add00e
Johannes Hahn <johanneshahnrostock@googlemail.com>
2024-11-21 23:40:08 +0100

    add StepFunction

---
+++ src/main/java/org/example/math/StepFunction.java
@@ -0,0 +1,331 @@
+package org.example.math;
+
+import static org.example.math.Interval.Bound.unboundedAbove;
+import static org.example.math.Interval.Bound.unboundedBelow;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Objects;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+import org.example.math.Interval.Bound;
+
+/**
+ * Represents a "step function", i.e. a function whose domain can be divided into a finite number of intervals so that
+ * the function is constant on each interval.
+ *
+ * @param <T> the type of the domain.
+ */
+public class StepFunction<T, V> implements Function<T, V> {
+
+    // the nulls-first-comparator used in the TreeSet
+    private final Comparator<? super T> mapComparator;
+    // needed for equality testing, because Comparator.nullsFirst(..) does not override #equals()
+    private final Comparator<? super T> tComparator;
+    // invariants:
+    // 1. all values are non-null
+    // 2. all keys are non-null except the first one which represents -infinity
+    private final NavigableMap<T, V> values;
+
+    private StepFunction(TreeMap<T, V> values, Comparator<? super T> tComparator) {
+        this.values = values;
+        this.mapComparator = values.comparator();
+        this.tComparator = tComparator;
+        normalize();
+    }
+
+    private void normalize() {
+        var iterator = this.values.values().iterator();
+        V last = null;
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            if (current.equals(last)) { // implicit null check for all values in the map
+                // merge steps with equal values by removing intermediate entries
+                iterator.remove();
+                continue;
+            }
+            last = current;
+        }
+    }
+
+    /**
+     * @param <X> the type of the domain
+     * @return the constant zero function
+     * @see #constant(Object)
+     */
+    public static <X extends Comparable<? super X>> StepFunction<X, BigDecimal> zero() {
+        return constant(BigDecimal.ZERO);
+    }
+
+    /**
+     * @param <T> the type of the domain
+     * @return the constant one function
+     * @see #constant(Object)
+     */
+    public static <T extends Comparable<? super T>> StepFunction<T, BigDecimal> one() {
+        return constant(BigDecimal.ONE);
+    }
+
+    /**
+     * @param value the value of the function
+     * @param <T>   the type of the domain
+     * @return the constant function with the given value
+     */
+    public static <T extends Comparable<? super T>, V> StepFunction<T, V> constant(V value) {
+        return constant(value, Comparator.naturalOrder());
+    }
+
+    /**
+     * @param value       the value of the function
+     * @param tComparator the order of the domain
+     * @param <T>         the type of the domain
+     * @return the constant function with the given value where the domain is ordered by the given comparator.
+     */
+    public static <T, V> StepFunction<T, V> constant(V value, Comparator<? super T> tComparator) {
+        Objects.requireNonNull(value);
+        Objects.requireNonNull(tComparator);
+        return new StepFunction<>(newConstantValues(value, tComparator), tComparator);
+    }
+
+    private static <T, V> TreeMap<T, V> newConstantValues(V value, Comparator<? super T> tComparator) {
+        TreeMap<T, V> map = new TreeMap<>(Comparator.nullsFirst(tComparator));
+        map.put(null, value);
+        return map;
+    }
+
+    /**
+     * @param interval an interval
+     * @param value    a value
+     * @param <X>      the type of the domain
+     * @return a step function that has the value {@link BigDecimal#ZERO} outside of the given interval and
+     * {@code value} within.
+     */
+    public static <X> StepFunction<X, BigDecimal> singleStep(Interval<X> interval, BigDecimal value) {
+        Objects.requireNonNull(interval);
+        Objects.requireNonNull(value);
+
+        final TreeMap<X, BigDecimal> newValues = newConstantValues(BigDecimal.ZERO, interval.comparator());
+        if (!interval.isEmpty()) {
+            // if start is infinite, then it is negative infinity which we represent in this class with null
+            X start = interval.start().getValue().orElse(null);
+            newValues.put(start, value);
+
+            // if end is not infinite, then we put a zero after the interval
+            interval.end().getValue().ifPresent(e -> newValues.put(e, BigDecimal.ZERO));
+        }
+        return new StepFunction<>(newValues, interval.comparator());
+    }
+
+    @Override
+    public V apply(T t) {
+        Objects.requireNonNull(t);
+        return values.floorEntry(t) // always non-null because null is always in the map and the smallest object
+                     .getValue();
+    }
+
+    /**
+     * @return {@code true} iff this function is constant.
+     */
+    public boolean isConstant() {
+        return values.size() == 1;
+    }
+
+    /**
+     * @param value a test value
+     * @return {@code true} iff this function is constant equal to the given value.
+     */
+    public boolean isConstant(final V value) {
+        return isConstant() && values.get(null).equals(value);
+    }
+
+    /**
+     * @return the list of this function's values, ordered by the given order on the domain beginning with the value at
+     * "-infinity" and ending with the value at "+infinity".
+     */
+    public List<V> values() {
+        return List.copyOf(values.values());
+    }
+
+    /**
+     * Returns the set where this step function differs from the given value, expressed as a list of disjoint
+     * {@link Interval}s. The returned list is empty iff this differs nowhere from the given value, i.e. if this
+     * function is constant and everywhere equal to the given value. {@code null} is returned to represent the whole
+     * domain {@code X} (there is no {@link Interval} that would correspond to that if {@code X} is unbounded in the
+     * given order).
+     *
+     * @param zero the test value
+     * @return a
+     */
+    public List<Interval<T>> support(V zero) {
+        if (this.isConstant()) {
+            return this.isConstant(zero) ? List.of() : null;
+        }
+        List<Interval<T>> result = new ArrayList<>();
+
+        var iterator = this.values.entrySet().iterator();
+        var last = iterator.next();
+
+        Interval.Bound<T> nonZeroSince = last.getValue().equals(zero) ? null : unboundedBelow();
+
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            if (current.getValue().equals(zero)) {
+                if (nonZeroSince != null) {
+                    result.add(new Interval<>(nonZeroSince, Bound.of(current.getKey()), tComparator));
+                    nonZeroSince = null;
+                }
+            } else if (nonZeroSince == null) {
+                nonZeroSince = Bound.of(current.getKey());
+            }
+            last = current;
+        }
+
+        if (nonZeroSince != null && !last.getValue().equals(zero)) {
+            result.add(new Interval<>(nonZeroSince, unboundedAbove(), tComparator));
+        }
+
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <V1> StepFunction<T, V1> andThen(Function<? super V, ? extends V1> f) {
+        // unchecked casts so that we can use the more efficient copy constructor here
+        TreeMap<T, Object> result = new TreeMap<>(this.values);
+        result.replaceAll((k, v) -> f.apply((V) v));
+        return new StepFunction<>((TreeMap<T, V1>) result, tComparator);
+    }
+
+
+    /**
+     * @param f   the operator
+     * @param <X> the type of the domain
+     * @param <Y> the type of the values
+     * @return a {@link UnaryOperator} that, given a step-function {@code s}, returns the step-function that results
+     * from applying the given operator to the values, i.e. the function {@code x->f(s(x))}.
+     */
+    public static <X, Y> UnaryOperator<StepFunction<X, Y>> pointwise(UnaryOperator<Y> f) {
+        return s -> s.andThen(f);
+    }
+
+    /**
+     * @param f   the operator
+     * @param <X> the type of the domain
+     * @param <Y> the type of the values
+     * @return a {@link BinaryOperator} that, given two step-functions {@code s1, s2}, returns the step-function that
+     * results from applying the given operator to the values, i.e. the function {@code x->f(s1(x),s2(x))}.
+     */
+    public static <X, Y> BinaryOperator<StepFunction<X, Y>> pointwise(BinaryOperator<Y> f) {
+        return (s1, s2) -> {
+            requireCompatibility(s1, s2);
+            return new StepFunction<>(
+                    new Unionizer<>(s1.values, s2.values, s1.mapComparator).forEachStep(f), s1.tComparator);
+        };
+    }
+
+    private static <T, V> void requireCompatibility(StepFunction<T, V> s1, StepFunction<T, V> s2) {
+        if (!s1.tComparator.equals(s2.tComparator)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        return o instanceof StepFunction<?, ?> that && this.tComparator.equals(that.tComparator) && this.values.equals(
+                that.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tComparator, values);
+    }
+
+    /**
+     * Iterates through sorted maps from top to bottom, producing a union of both maps
+     */
+    private static class Unionizer<T, V> {
+
+        private final Iterator<Map.Entry<T, V>> aIter;
+        private final Iterator<Map.Entry<T, V>> bIter;
+
+        private final Comparator<? super T> mapComparator;
+        private Map.Entry<T, V> aEntry;
+        private Map.Entry<T, V> bEntry;
+
+        Unionizer(SortedMap<T, V> a, SortedMap<T, V> b, Comparator<? super T> mapComparator) {
+            // we iterate in decreasing order through both maps
+            aIter = a.sequencedEntrySet().reversed().iterator();
+            assert aIter.hasNext();
+            aEntry = aIter.next();
+
+            bIter = b.sequencedEntrySet().reversed().iterator();
+            assert bIter.hasNext();
+            bEntry = bIter.next();
+            this.mapComparator = mapComparator;
+        }
+
+        TreeMap<T, V> forEachStep(BinaryOperator<V> action) {
+            final TreeMap<T, V> result = new TreeMap<>(mapComparator);
+
+            // zig-zag algorithm
+            boolean moreToDo = true;
+            while (moreToDo) {
+                V currentValue = action.apply(aEntry.getValue(), bEntry.getValue());
+
+                int comp = mapComparator.compare(aEntry.getKey(), bEntry.getKey());
+                T currentKey;
+                if (comp < 0) {
+                    // A < B => use B key & decrease B
+                    currentKey = bEntry.getKey();
+                    moreToDo = decreaseB();
+                } else if (comp == 0) {
+                    // A == B => use any key & decrease both
+                    //
+                    // Note that both maps have null as their lowest key, so we will inevitably end the loop with this
+                    // branch.
+                    currentKey = aEntry.getKey();
+                    boolean moreA = decreaseA();
+                    boolean moreB = decreaseB();
+                    moreToDo = moreA && moreB;
+                } else {
+                    // B < A => use A key & decrease left
+                    currentKey = aEntry.getKey();
+                    moreToDo = decreaseA();
+                }
+                result.put(currentKey, currentValue);
+            }
+
+            return result;
+        }
+
+        private boolean decreaseA() {
+            if (aIter.hasNext()) {
+                aEntry = aIter.next();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private boolean decreaseB() {
+            if (bIter.hasNext()) {
+                bEntry = bIter.next();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/test/java/org/example/math/StepFunctionTest.java
@@ -0,0 +1,236 @@
+package org.example.math;
+
+import static java.math.BigDecimal.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.example.math.StepFunction.*;
+
+import java.math.BigDecimal;
+import java.util.function.BinaryOperator;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class StepFunctionTest {
+    static final BinaryOperator<StepFunction<Integer, BigDecimal>> ADDITION = StepFunction.pointwise(BigDecimal::add);
+
+    @Nested
+    class TestConstants {
+
+        @Test
+        void constantZeroIsZero() {
+            var zero = constant(ZERO);
+
+            assertThat(zero.isConstant()).isTrue();
+            assertThat(zero.isConstant(ZERO)).isTrue();
+            assertThat(zero.values()).containsExactly(ZERO);
+        }
+
+        @Test
+        void constantOneIsNotZero() {
+            StepFunction<Integer, BigDecimal> zero = constant(ONE);
+
+            assertThat(zero.isConstant()).isTrue();
+            assertThat(zero.isConstant(ONE)).isTrue();
+            assertThat(zero.values()).containsExactly(ONE);
+        }
+
+        @Test
+        void simpleStepIsNotZero() {
+            var f = singleStep(Interval.of(0, 1), BigDecimal.valueOf(42));
+
+            assertThat(f.isConstant()).isFalse();
+            assertThat(f.values()).containsExactly(ZERO, BigDecimal.valueOf(42), ZERO);
+        }
+
+        @Test
+        void stepFunctionWithEmptyIntervalIsZero() {
+            var f = singleStep(Interval.of(1, 0), BigDecimal.valueOf(42));
+
+            assertThat(f.isConstant()).isTrue();
+            assertThat(f.isConstant(ZERO)).isTrue();
+            assertThat(f.values()).containsExactly(ZERO);
+        }
+    }
+
+    @Nested
+    class TestAddition {
+
+        private final StepFunction<Integer, BigDecimal> f1 = singleStep(Interval.of(1, 5), BigDecimal.valueOf(42));
+        private final StepFunction<Integer, BigDecimal> f2 = singleStep(Interval.of(3, 7), BigDecimal.valueOf(5));
+        private final StepFunction<Integer, BigDecimal> f3 = singleStep(Interval.of(1, 4), BigDecimal.valueOf(-40));
+        private final StepFunction<Integer, BigDecimal> f4 = singleStep(Interval.of(5, 7), BigDecimal.valueOf(-5));
+        private final StepFunction<Integer, BigDecimal> f5 = singleStep(Interval.of(-3, -2), BigDecimal.valueOf(1));
+
+        @Test
+        void addZero() {
+            var f = singleStep(Interval.of(0, 1), BigDecimal.valueOf(42));
+
+            StepFunction<Integer, BigDecimal> zero = zero();
+            assertThat(ADDITION.apply(f, zero)).as("f+0=f").isEqualTo(f);
+            assertThat(ADDITION.apply(zero, f)).as("0+f=f").isEqualTo(f);
+        }
+
+        @Test
+        void overlappingIntervals() {
+            var f1plusf2 = ADDITION.apply(f1, f2);
+
+            assertSoftly(softly -> {
+                softly.assertThat(f1plusf2.values())
+                      .containsExactly(ZERO, BigDecimal.valueOf(42), BigDecimal.valueOf(47), BigDecimal.valueOf(5),
+                              ZERO);
+
+                softly.assertThat(f1plusf2.apply(0)).as("(f_1+f_2)(0)= 0").isEqualTo(ZERO);
+                softly.assertThat(f1plusf2.apply(1)).as("(f_1+f_2)(1)=42").isEqualTo(BigDecimal.valueOf(42));
+                softly.assertThat(f1plusf2.apply(2)).as("(f_1+f_2)(2)=42").isEqualTo(BigDecimal.valueOf(42));
+                softly.assertThat(f1plusf2.apply(3)).as("(f_1+f_2)(3)=47").isEqualTo(BigDecimal.valueOf(47));
+                softly.assertThat(f1plusf2.apply(4)).as("(f_1+f_2)(4)=47").isEqualTo(BigDecimal.valueOf(47));
+                softly.assertThat(f1plusf2.apply(5)).as("(f_1+f_2)(5)= 5").isEqualTo(BigDecimal.valueOf(5));
+                softly.assertThat(f1plusf2.apply(6)).as("(f_1+f_2)(6)= 5").isEqualTo(BigDecimal.valueOf(5));
+                softly.assertThat(f1plusf2.apply(7)).as("(f_1+f_2)(7)= 0").isEqualTo(ZERO);
+                softly.assertThat(f1plusf2.apply(7)).as("(f_1+f_2)(8)= 0").isEqualTo(ZERO);
+            });
+        }
+
+        @Test
+        void intervalsWithSameStart() {
+            var sumF1toF3 = ADDITION.apply(ADDITION.apply(f1, f2), f3);
+
+            assertSoftly(softly -> {
+
+                softly.assertThat(sumF1toF3.values())
+                      .containsExactly(ZERO, TWO, BigDecimal.valueOf(7), BigDecimal.valueOf(47), BigDecimal.valueOf(5),
+                              ZERO);
+
+                softly.assertThat(sumF1toF3.apply(0)).as("(f_1+f_2+f_3)(0)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF3.apply(1)).as("(f_1+f_2+f_3)(1)= 2").isEqualTo(TWO);
+                softly.assertThat(sumF1toF3.apply(2)).as("(f_1+f_2+f_3)(2)= 2").isEqualTo(TWO);
+                softly.assertThat(sumF1toF3.apply(3)).as("(f_1+f_2+f_3)(3)= 7").isEqualTo(BigDecimal.valueOf(7));
+                softly.assertThat(sumF1toF3.apply(4)).as("(f_1+f_2+f_3)(4)=47").isEqualTo(BigDecimal.valueOf(47));
+                softly.assertThat(sumF1toF3.apply(5)).as("(f_1+f_2+f_3)(5)= 5").isEqualTo(BigDecimal.valueOf(5));
+                softly.assertThat(sumF1toF3.apply(6)).as("(f_1+f_2+f_3)(6)= 5").isEqualTo(BigDecimal.valueOf(5));
+                softly.assertThat(sumF1toF3.apply(7)).as("(f_1+f_2+f_3)(7)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF3.apply(7)).as("(f_1+f_2+f_3)(8)= 0").isEqualTo(ZERO);
+            });
+        }
+
+        @Test
+        void intervalsWithSameEnd() {
+            StepFunction<Integer, BigDecimal> s1 = ADDITION.apply(f1, f2);
+            StepFunction<Integer, BigDecimal> s3 = ADDITION.apply(s1, f3);
+            var sumF1toF4 = ADDITION.apply(s3, f4);
+
+            assertSoftly(softly -> {
+                softly.assertThat(sumF1toF4.values())
+                      .containsExactly(ZERO, TWO, BigDecimal.valueOf(7), BigDecimal.valueOf(47), ZERO);
+
+                softly.assertThat(sumF1toF4.apply(0)).as("(f_1+..+f_4)(0)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF4.apply(1)).as("(f_1+..+f_4)(1)= 2").isEqualTo(TWO);
+                softly.assertThat(sumF1toF4.apply(2)).as("(f_1+..+f_4)(2)= 2").isEqualTo(TWO);
+                softly.assertThat(sumF1toF4.apply(3)).as("(f_1+..+f_4)(3)= 7").isEqualTo(BigDecimal.valueOf(7));
+                softly.assertThat(sumF1toF4.apply(4)).as("(f_1+..+f_4)(4)=47").isEqualTo(BigDecimal.valueOf(47));
+                softly.assertThat(sumF1toF4.apply(5)).as("(f_1+..+f_4)(5)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF4.apply(6)).as("(f_1+..+f_4)(6)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF4.apply(7)).as("(f_1+..+f_4)(7)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF4.apply(7)).as("(f_1+..+f_4)(8)= 0").isEqualTo(ZERO);
+            });
+        }
+
+        @Test
+        void disjointIntervals() {
+            StepFunction<Integer, BigDecimal> s1 = ADDITION.apply(f1, f2);
+            StepFunction<Integer, BigDecimal> s3 = ADDITION.apply(s1, f3);
+            StepFunction<Integer, BigDecimal> s4 = ADDITION.apply(s3, f4);
+            var sumF1toF5 = ADDITION.apply(s4, f5);
+            assertSoftly(softly -> {
+                softly.assertThat(sumF1toF5.values())
+                      .containsExactly(ZERO, ONE, ZERO, TWO, BigDecimal.valueOf(7), BigDecimal.valueOf(47), ZERO);
+
+                softly.assertThat(sumF1toF5.apply(-4)).as("(f_1+..+f_5)(-4)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(-3)).as("(f_1+..+f_5)(-3)= 0").isEqualTo(ONE);
+                softly.assertThat(sumF1toF5.apply(-2)).as("(f_1+..+f_5)(-2)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(-1)).as("(f_1+..+f_5)(-1)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(0)).as("(f_1+..+f_5)( 0)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(1)).as("(f_1+..+f_5)( 1)= 2").isEqualTo(TWO);
+                softly.assertThat(sumF1toF5.apply(2)).as("(f_1+..+f_5)( 2)= 2").isEqualTo(TWO);
+                softly.assertThat(sumF1toF5.apply(3)).as("(f_1+..+f_5)( 3)= 7").isEqualTo(BigDecimal.valueOf(7));
+                softly.assertThat(sumF1toF5.apply(4)).as("(f_1+..+f_5)( 4)=47").isEqualTo(BigDecimal.valueOf(47));
+                softly.assertThat(sumF1toF5.apply(5)).as("(f_1+..+f_5)( 5)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(6)).as("(f_1+..+f_5)( 6)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(7)).as("(f_1+..+f_5)( 7)= 0").isEqualTo(ZERO);
+                softly.assertThat(sumF1toF5.apply(7)).as("(f_1+..+f_5)( 8)= 0").isEqualTo(ZERO);
+            });
+        }
+
+        @Test
+        void negation() {
+            var result = ADDITION.apply(f1, f1.andThen(BigDecimal::negate));
+
+            assertThat(result.isConstant(ZERO)).isTrue();
+        }
+    }
+
+    @Nested
+    class TestSupport {
+
+        @Test
+        void zero() {
+            var f = StepFunction.zero();
+
+            assertThat(f.support(ZERO)).isEmpty();
+        }
+
+        @Test
+        void singleStep() {
+            final Interval<Integer> interval = Interval.of(1, 3);
+            var f = StepFunction.singleStep(interval, ONE);
+
+            assertThat(f.support(ZERO)).containsExactly(interval);
+        }
+
+        @Test
+        void twoAdjacentSteps() {
+            StepFunction<Integer, BigDecimal> s1 = StepFunction.singleStep(Interval.of(1, 2), ONE);
+            StepFunction<Integer, BigDecimal> s2 = StepFunction.singleStep(Interval.of(2, 3), BigDecimal.TWO);
+            var f = StepFunction.<Integer, BigDecimal>pointwise(BigDecimal::add).apply(s1, s2);
+
+            assertThat(f.support(ZERO)).containsExactly(Interval.of(1, 3));
+        }
+
+        @Test
+        void sumOfTwoOverlappingSteps() {
+            StepFunction<Integer, BigDecimal> s1 = StepFunction.singleStep(Interval.of(1, 3), ONE);
+            StepFunction<Integer, BigDecimal> s2 = StepFunction.singleStep(Interval.of(2, 4), BigDecimal.TWO);
+            var f = StepFunction.<Integer, BigDecimal>pointwise(BigDecimal::add).apply(s1, s2);
+            // f equal to singleStep([1,2[, 1) + singleStep([2,3[, 3) + singleStep([3,4[, 2]
+
+            assertThat(f.support(ZERO)).containsExactly(Interval.of(1, 4));
+        }
+
+        @Test
+        void twoNonAdjacentSteps() {
+            StepFunction<Integer, BigDecimal> s1 = StepFunction.singleStep(Interval.of(1, 2), ONE);
+            StepFunction<Integer, BigDecimal> s2 = StepFunction.singleStep(Interval.of(3, 4), BigDecimal.TWO);
+            var f = StepFunction.<Integer, BigDecimal>pointwise(BigDecimal::add).apply(s1, s2);
+
+            assertThat(f.support(ZERO)).containsExactly(Interval.of(1, 2), Interval.of(3, 4));
+        }
+
+        @Test
+        void constant() {
+            var f = StepFunction.constant(ONE);
+
+            assertThat(f.support(ZERO)).isNull();
+        }
+
+        @Test
+        void almostConstantExceptForOneBit() {
+            final Interval<Integer> leftPiece = Interval.of(Interval.Bound.unboundedBelow(), Interval.Bound.of(0));
+            final Interval<Integer> rightPiece = Interval.of(Interval.Bound.of(1), Interval.Bound.unboundedAbove());
+
+            var f = ADDITION.apply(StepFunction.singleStep(leftPiece, ONE), StepFunction.singleStep(rightPiece, ONE));
+
+            assertThat(f.support(ZERO)).containsExactly(leftPiece, rightPiece);
+        }
+    }
+}
